323e62dd60b25de9c5a7521471217ac332fc6100
BeyzaBolat <beyzabolat_@outlook.com>
2023-09-12 17:50:26 +0300

    update

---
+++ ecommerceWebMvc/Controllers/RoleAdminController.cs
@@ -0,0 +1,107 @@
+﻿using ecommerceWebMvc.Models.Classes;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ecommerceWebMvc.Controllers
+{
+    public class RoleAdminController : Controller
+    {
+        // GET: RoleAdmin
+
+        private RoleManager<IdentityRole> roleManager;
+        private UserManager<ApplicationUser> userManager;
+
+        public RoleAdminController()
+        {
+            roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new IdentityDataContext()));
+            userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new IdentityDataContext()));
+            //var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+
+        }
+        public ActionResult Index()
+        {
+            return View(roleManager.Roles);
+        }
+
+
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(string name)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = roleManager.Create(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+            }
+            return View(name);
+        }
+        [HttpPost]
+        public ActionResult Delete(string id)
+        {
+            var role = roleManager.FindById(id);
+            if (role != null)
+            {
+                var result = roleManager.Delete(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View("Error", result.Errors);
+                }
+            }
+            else
+            {
+                return View("Error", new string[] { "Role Bulunamadı." });
+            }
+
+        }
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            var role = roleManager.FindById(id);
+            var members = new List<ApplicationUser>();
+            var nonMembers = new List<ApplicationUser>();
+
+
+            foreach (var user in userManager.Users)
+            {
+                var list = userManager.IsInRole(user.Id, role.Name) ?
+                    members : nonMembers;
+                list.Add(user);
+
+            }
+            return View(new RoleEditModel()
+            {
+                Role=role,
+                Members=members,
+                NonMembers=nonMembers
+            });
+
+        }
+    }
+}
